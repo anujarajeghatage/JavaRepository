aa84e7684135640c7da6391dee01ca054480a6f5
anujarajeghatage <anuja.a.rajeghatage@gmail.com>
2023-01-18 14:26:02 +0530

    java basics

---
+++ src/com/programing/scn.java
@@ -0,0 +1,15 @@
+package com.programing;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class scn {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("name is");
+        String name=sc.nextLine();
+        System.out.println(name);
+
+    }
+}
